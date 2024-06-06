50e8c05ebfe4b46271ff647665a7f8f969734549
nvhlam2211 <hoanglam24.qc@gmail.com>
2024-06-07 00:27:52 +0700

    Nop bai tap buoi 21

---
+++ Day-21/ex03.js
@@ -0,0 +1,8 @@
+var arrThird = [1, 1, 2, 5, 6, 7, 7];
+var newArr = arrThird.reduce(function(number,nextNumber){
+    if(!number.includes(nextNumber)){
+        number.push(nextNumber);
+    }
+    return number;
+}, []);
+console.log(`Mảng sau khi lọc là: ${newArr}`);
